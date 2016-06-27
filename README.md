2be90c4402af353ff676f419a0339c0060fcebd2
Luiz Fernando <luizfernandorg@gmail.com>
2016-06-26 22:09:33 -0300

    readme adjust and add comments

--- README.md
+++ README.md
@@ -4,17 +4,39 @@ This is a example based on Tim O'kellys's code, about calendar generate
 
 ## Code Example
 
-For now the library provide tree pricipal methods and one auxilary method:
-  > buildMonth(date)
-  ## This method take an object Date to generate the hole month for the calendar,
-  retornig an array with all date present in respective calendar month.
-  all itens in the array will be an object Date type
+  '''
+  var calendar = require('./index')
 
-  > buildYear(year)
-  ## This method take a type number that represent the year, and will return the follow object
-  {
-   'monthName': [dates objects, ...] // all months will have the name and the array created by buildMonth
+  var months = calendar.buildYear(new Date().getFullYear());
+
+  for(var k in months){
+      var lines = '';
+      var col = 0; //go to 6
+
+      //print the name of month
+      lines += k + ':\n';
+
+      //loop for all the date in k month
+      months[k].forEach(function(date){
+          var mDate = new Date(date).getDate();
+
+          //all the dates for the respective day
+          lines += mDate;
+
+          //if pass through all days of week create a new line
+          if(col === 6){
+              lines += '\n';
+              col = 0;
+          }else{
+              lines += '\t';
+              col++;
+          }
+      });
+
+      //show the month in 6x7 format
+      console.log(lines);
   }
+  '''
 
 ## Motivation
 
@@ -22,7 +44,11 @@ A short description of the motivation behind the creation and maintenance of the
 
 ## Installation
 
-Provide code examples and explanations of how to get the project.
+Will need install node
+
+'''
+npm install node
+'''
 
 ## API Reference
 
--- index.js
+++ index.js
@@ -16,13 +16,41 @@ module.exports = (function(){
         'November',
         'December',
     ]
+
+    /*
+     * Receive a date object to get the fist date for the month calendar
+     * @param date - is a Date object
+     * @return a new Date Object
+     *
+     * The month calendar is 6 x 7
+     * Example:
+     *  for 2016 January.
+     *  the return will be 2015, December 27
+     *  because is the first date in January Calendar
+     */
     var firstCalendarDate = function(date){
-        var newDate = new Date(date.getFullYear(), date.getMonth(), 1);
-        var day = newDate.getDay();
-        var dDate = newDate.getDate() - day;
-        return new Date(date.setDate(dDate));
+      //Generate the first date for the month
+      var newDate = new Date(date.getFullYear(), date.getMonth(), 1);
+
+      var day = newDate.getDay();
+
+      //get the date difference
+      var dDate = newDate.getDate() - day;
+
+      //return a new Date object
+      return new Date(date.setDate(dDate));
     }
 
+    /*
+     *
+     * @param date  - must be a Date object
+     *
+     * This method will call the firstCalendarDate to get
+     * the first date for that calendar
+     *
+     * the return will be an array with all Date objects for the calendar
+     * precisely 42 dates
+     */
     var buildMonth = function(date){
         var day = 0;
         var date = firstCalendarDate(date);
@@ -33,18 +61,35 @@ module.exports = (function(){
         }
         return dates;
     }
+    /*
+     * @param date  - a date object
+     *
+     * return a string with the name of the month
+     */
     var getMonth = function(date){
         return myMonths[date.getMonth()];
     }
+    /*
+     * @param year - a year in number format
+     *
+     * Will build all months in that year calling buildMonth method
+     *
+     * The return will be an object with a name of month an array for this name:
+     * {
+     *  'january': [DateObject, DateObject, ...],
+     *  ...
+     * }
+     */
     var buildYear = function(year){
         var months = {};
         for(var month = 0; month < 12; month++){
-            months[myMonths[month]] = buildMonth(new Date(year, month, 1));
+          //generate a new month with all 42 dates
+          months[myMonths[month]] = buildMonth(new Date(year, month, 1));
         }
         return months;
     }
+
     return {
-        firstCalendarDate: firstCalendarDate,
         getMonth: getMonth,
         buildMonth: buildMonth,
         buildYear: buildYear
