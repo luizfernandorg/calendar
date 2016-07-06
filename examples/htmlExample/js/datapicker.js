13b3b3cae3fecaa1f7e4719e2452ce3ec1b42830
luizfernandorg <luizfernandorg@gmail.com>
2016-07-06 18:45:44 -0300

    update method firstCalendarDate

--- examples/htmlExample/js/datapicker.js
+++ examples/htmlExample/js/datapicker.js
@@ -1,9 +1,43 @@
 var currentDate = new Date();
+var today = new Date();
+var dates = [];
+
 var updateDates = function(){
   var data = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
-  var dates = calendar.buildMonth(data);
-  console.log(calendar.getMonth(currentDate.getMonth()));
+
+  dates = calendar.buildMonth(data);
+
+  var dateLi = $('ul.dates li a');
+
+  setMonthName();
+
+  for(var d = 0; d < 42; d++){
+    if($(dateLi[d]).hasClass('outCurrentMonth')){
+      $(dateLi[d]).removeClass('outCurrentMonth');
+    }
+
+    if(dates[d].getMonth() !== currentDate.getMonth()){
+      $(dateLi[d]).addClass('outCurrentMonth');
+    }
+
+    if($(dateLi[d]).parent().hasClass('today')){
+      $(dateLi[d]).parent().removeClass('today');
+    }
+
+    if(dates[d].getDate() === today.getDate() && dates[d].getMonth() === today.getMonth()){
+      $(dateLi[d]).parent().addClass('today');
+    }
+
+    $(dateLi[d]).attr('data-role', dates[d].getMonth());
+
+    dateLi[d].innerHTML = dates[d].getDate();
+  }
 }
+var setMonthName = function(){
+  $('.month-name').text(calendar.getMonth(currentDate.getMonth()) + ', ' + currentDate.getFullYear());
+}
+
+updateDates();
 
 $('.month-left').on('click', function(event){
   event.preventDefault();
@@ -16,3 +50,16 @@ $('.month-right').on('click', function(event){
   currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth()+1, currentDate.getDate());
   updateDates();
 });
+
+$('ul.dates li a').on('click',function(event){
+  event.preventDefault();
+  var dataRoleMonth = parseInt($(this).attr('data-role'));
+  var isCurrentMonth = dataRoleMonth === currentDate.getMonth();
+  if(!isCurrentMonth){
+    console.log(currentDate.getFullYear(), dataRoleMonth, parseInt($(this).text()));
+    currentDate = new Date(currentDate.getFullYear(), dataRoleMonth, parseInt($(this).text()) );
+    console.log(currentDate);
+    updateDates();
+  }
+
+})
